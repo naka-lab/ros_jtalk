e1ef1c7d8521f770b510f46f9c4e3ff7437cd5dc
なかとも <20672947+naka-tomo@users.noreply.github.com>
2023-04-08 07:41:11 +0900

    mac対応

--- scripts/open_jtalk.py
+++ scripts/open_jtalk.py
@@ -3,14 +3,18 @@
 import rospy
 import os
 import std_msgs.msg
+import platform
 
 
 class OpenJTalk():
     def __init__(self):
         rospy.on_shutdown(self.shutdown)
         rospy.Subscriber("jtalk/utterance", std_msgs.msg.String, self.say)
-        self._open_jtalk_script = os.path.join(
-            os.path.dirname(__file__), "open_jtalk.sh")
+
+        if platform.system()=="Darwin": # Macの場合
+            self._open_jtalk_script = os.path.join(os.path.dirname(__file__), "open_jtalk_mac.sh")
+        else:
+            self._open_jtalk_script = os.path.join(os.path.dirname(__file__), "open_jtalk.sh")
 
         rospy.spin()
 
---
+++ scripts/open_jtalk_mac.sh
@@ -0,0 +1,29 @@
+# インストール
+# brew install open-jtalk
+
+# 他のディレクトリから実行されたときに問題ないように
+# このディレクトリに cd する
+# 最後にはこのディレクトリに戻ってくる
+cwd=`pwd`
+this_dir=`dirname ${0}`
+cd $this_dir
+echo $1 > speech.txt
+
+open_jtalk \
+-m /opt/homebrew/Cellar/open-jtalk/1.11/voice/mei/mei_happy.htsvoice \
+-x naist-jdic \
+-s 48000 \
+-p 240 \
+-a 0.53 \
+-u 0.3 \
+-jm 0.8 \
+-jf 0.4 \
+-z 48000 \
+-ow _hoge.wav \
+-ot _log.txt \
+speech.txt
+
+afplay _hoge.wav
+
+# 元のディレクトリに戻る
+cd $cwd
